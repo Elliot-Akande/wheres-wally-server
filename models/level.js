9b6a30150e83f62b3b09eac03e484dd57c5486e6
Elliot Akande <elbo.lynch@gmail.com>
2024-01-26 11:39:09 +0000

    Add imageUrl and selection size to schema

--- models/level.js
+++ models/level.js
@@ -3,12 +3,15 @@ const Schema = mongoose.Schema;
 
 const answerSchema = new Schema({
   character: { type: String, required: true },
+  imageUrl: { type: String, required: true },
   xCoord: { type: Number, required: true, min: 0 },
   yCoord: { type: Number, required: true, min: 0 },
+  size: { type: Number, required: true, min: 0, default: 50 },
 });
 
 const LevelSchema = new Schema({
   levelNum: { type: Number, required: true, unique: true, min: 1 },
+  imageUrl: { type: String, required: true },
   answers: { type: [answerSchema], required: true },
 });
 
