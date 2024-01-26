4b02f52d01c9f63b4eb2b9ae2a1b1320a597fe06
Elliot Akande <elbo.lynch@gmail.com>
2024-01-26 12:54:22 +0000

    Change levelNum uri param to levelId

--- routes/levels.js
+++ routes/levels.js
@@ -6,18 +6,18 @@ const levelController = require("../controllers/levelController");
 router.get("/", levelController.levelList);
 
 // Get level details
-router.get("/:levelNum", levelController.levelDetails);
+router.get("/:levelId", levelController.levelDetails);
 
 // Get level leaderboard
-router.get("/:levelNum/leaderboard", levelController.leaderboardGET);
+router.get("/:levelId/leaderboard", levelController.leaderboardGET);
 
 // POST score to level leaderboard
-router.post("/:levelNum/leaderboard", levelController.leaderboardPOST);
+router.post("/:levelId/leaderboard", levelController.leaderboardPOST);
 
 // POST request for checking an answer is correct
-router.post("/:levelNum/check-answer", levelController.checkAnswer);
+router.post("/:levelId/check-answer", levelController.checkAnswer);
 
 // POST request for checking a level is complete
-router.post("/:levelNum/check-complete", levelController.checkComplete);
+router.post("/:levelId/check-complete", levelController.checkComplete);
 
 module.exports = router;
