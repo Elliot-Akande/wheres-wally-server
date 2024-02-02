c60a0536aa942ffdccd9fbfc30941c33322add69
Elliot Akande <elbo.lynch@gmail.com>
2024-02-02 23:33:47 +0000

    Implement leaderboard post route controller

--- controllers/levelController.js
+++ controllers/levelController.js
@@ -2,10 +2,31 @@ const Level = require("../models/level");
 const Leaderboard = require("../models/leaderboard");
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
-const { v4: uuid } = require("uuid");
 const { body, validationResult } = require("express-validator");
 
-// Get list of all levels
+const parseJWT = (req, res, next) => {
+  // Check JWT is present
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader === "undefined") {
+    const err = new Error("Forbidden");
+    err.status = 403;
+    return next(err);
+  }
+
+  // Verify token
+  const token = bearerHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_KEY, (err, data) => {
+    if (err) {
+      const error = new Error("Invalid token");
+      error.status = 403;
+      return next(error);
+    }
+    req.token = data;
+    next();
+  });
+};
+
+// GET list of all levels
 exports.levelList = asyncHandler(async (req, res, next) => {
   const levels = await Level.find({}, "levelNum imageUrl")
     .sort({ levelNum: 1 })
@@ -14,7 +35,7 @@ exports.levelList = asyncHandler(async (req, res, next) => {
   res.json(levels);
 });
 
-// Get level details
+// GET level details
 exports.levelDetails = asyncHandler(async (req, res, next) => {
   const data = await Level.findById(req.params.levelId).exec();
 
@@ -24,13 +45,13 @@ exports.levelDetails = asyncHandler(async (req, res, next) => {
     return next(err);
   }
 
-  jwt.sign({}, process.env.JWT_KEY, (err, token) => {
+  jwt.sign({ isComplete: false }, process.env.JWT_KEY, (err, token) => {
     if (err) return next(err);
     return res.json({ token, data });
   });
 });
 
-// Get level leaderboard
+// GET level leaderboard
 exports.leaderboardGET = asyncHandler(async (req, res, next) => {
   const leaderboard = await Leaderboard.findOne({
     level: req.params.levelId,
@@ -46,9 +67,65 @@ exports.leaderboardGET = asyncHandler(async (req, res, next) => {
 });
 
 // POST score to level leaderboard
-exports.leaderboardPOST = asyncHandler(async (req, res, next) => {
-  res.json({ msg: "Not yet implemented" });
-});
+exports.leaderboardPOST = [
+  body("name", "Name must be present").trim().isLength({ min: 1 }).escape(),
+  parseJWT,
+  asyncHandler(async (req, res, next) => {
+    // Check level is complete
+    if (!req.token.isComplete) {
+      const err = new Error("Forbidden");
+      err.status = 403;
+      return next(err);
+    }
+
+    // Validate request body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Check level exists
+    const leaderboard = await Leaderboard.findOne({
+      level: req.params.levelId,
+    }).exec();
+    if (!leaderboard) {
+      const err = new Error("Level not found");
+      err.status = 404;
+      return next(err);
+    }
+
+    // Recursive binary search
+    const insertIntoLeaderboard = (
+      user,
+      start = 0,
+      end = leaderboard.scores.length - 1
+    ) => {
+      const middle = Math.floor((start + end) / 2);
+      const currEntry = leaderboard.scores[middle];
+
+      if (start === end) {
+        if (currEntry.score > user.score) {
+          leaderboard.scores.splice(middle, 1, user, currEntry);
+        } else {
+          leaderboard.scores.splice(middle, 1, currEntry, user);
+        }
+        leaderboard.save();
+        return;
+      }
+
+      if (currEntry.score > user.score) {
+        return insertIntoLeaderboard(user, start, middle - 1);
+      }
+
+      if (currEntry.score < user.score) {
+        return insertIntoLeaderboard(user, middle + 1, end);
+      }
+    };
+
+    insertIntoLeaderboard({ name: req.body.name, score: req.token.score });
+    res.json(leaderboard);
+  }),
+];
 
 // POST request for checking an answer is correct
 exports.checkAnswer = [
@@ -82,14 +159,13 @@ exports.checkAnswer = [
       return next(err);
     }
 
-    const answer = data.answers[0];
-
     const validateCoords = (correctX, correctY, userX, userY, size) => {
       const deltaX = correctX - userX;
       const deltaY = correctY - userY;
       return deltaX ** 2 + deltaY ** 2 <= size ** 2;
     };
 
+    const answer = data.answers[0];
     res.json({
       isCorrect: validateCoords(
         answer.xCoord,
@@ -116,32 +192,9 @@ exports.checkComplete = [
     .trim()
     .isInt({ min: 0 }),
 
-  // Parse JWT
+  parseJWT,
   asyncHandler(async (req, res, next) => {
-    // Check JWT is present
-    const bearerHeader = req.headers.authorization;
-    if (typeof bearerHeader === "undefined") {
-      const err = new Error("Forbidden");
-      err.status = 403;
-      return next(err);
-    }
-
-    // Verify token
-    const token = bearerHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_KEY, (err, data) => {
-      if (err) {
-        const error = new Error("Invalid token");
-        error.status = 403;
-        return next(error);
-      }
-      req.token = token;
-      req.startTime = data.iat;
-      next();
-    });
-  }),
-
-  // Check level is complete
-  asyncHandler(async (req, res, next) => {
+    // Validate request body
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -176,15 +229,17 @@ exports.checkComplete = [
       }
     });
 
+    // Create new token
     const finishTime = Math.round(new Date().getTime() / 1000);
     jwt.sign(
-      { startTime: req.startTime, finishTime },
+      { isComplete: true, score: finishTime - req.token.iat },
       process.env.JWT_KEY,
       (err, newToken) => {
         if (err) return next(err);
+        // Send score data with new token
         return res.json({
           token: newToken,
-          score: finishTime - req.startTime,
+          score: finishTime - req.token.iat,
           isComplete: true,
         });
       }
